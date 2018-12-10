91a4c9126f778f35b5fc874560f0edad4ddd8459
Kyle McDonald <hello@kylemcd.com>
2018-12-09 18:15:11 -0600

    strip and escape chars

--- src/lambda/slackSubmit.js
+++ src/lambda/slackSubmit.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 export async function handler(event, context, callback){
   const payload = JSON.parse(event.body);
-  const email = payload.params.email;
+  const email = encodeURIComponent(payload.params.email.trim());
   const SLACK_TOKEN = process.env.SLACK_TOKEN;
   const SLACK_INVITE_ENDPOINT = 'https://slack.com/api/users.admin.invite';
   const toSlack = `email=${email}&token=${SLACK_TOKEN}&set_active=true`;
